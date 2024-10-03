03d7b1bfa7262fbfa4701cb7d7c1961b582bd45a
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-03 16:32:00 +0900

    Title: 문자열 정렬, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241003/문자열 정렬/string-sort.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String newS = new String(chars);
+        System.out.println(newS);
+    }
+}
