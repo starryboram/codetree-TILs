d23a6d5c4987bca38add176b5b3a539141d5be7c
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-09 13:27:59 +0900

    Title: 진수 to 진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+
+        n = getNumberByBinary(n, a);
+        n = getNumberToBinary(n, b);
+        System.out.println(n);
+    }
+
+    private static int getNumberToBinary(int n, int b) {
+        // 10 진수인 n 을 b 진수로 바꾸기
+        List<Integer> list = new ArrayList<>();
+        while (n != 0) {
+            list.add(n%b);
+            n /= b;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=list.size()-1; i>=0; i--) {
+            sb.append(list.get(i));
+        }
+        return Integer.parseInt(sb.toString());
+    }
+
+    private static int getNumberByBinary(int n, int a) {
+        // a 진수인 값 n 을 십진수로 바꾸기
+        int newNum = 0;
+        String num = String.valueOf(n);
+        for(int i=0; i<num.length(); i++){
+            newNum = (newNum * a) + Character.getNumericValue(num.charAt(i));
+        }
+        
+        return newNum;
+    }
+}
