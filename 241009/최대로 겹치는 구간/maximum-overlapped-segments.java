0b7a9cfa1d1cf8147e9a04a7ef56a97efc72562c
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-09 13:55:02 +0900

    Title: 최대로 겹치는 구간, Time: 264ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241009/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Panel {
+        int start;
+        int end;
+
+        public Panel(int start, int end) {
+            int base = 100;
+            this.start = base + start;
+            this.end = base + end;
+        }
+    }
+
+    static final int MAX = 200;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Panel> list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list.add(new Panel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        int[] line = new int[MAX];
+        list.forEach(panel -> {
+            for(int i=panel.start; i<panel.end; i++) {
+                line[i]++; 
+            }
+        });
+        
+        int ans = 0;
+        for(int i=0; i<line.length; i++) {
+            ans = Math.max(ans, line[i]);
+        }
+
+        System.out.println(ans);
+
+    }
+}
