3c7d89f8c575cc664b623174584819944280a527
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-11 14:16:55 +0900

    Title: 함수를 이용한 소수 판별, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241011/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String[] str = br.readLine().split(" ");
+            int num1 = Integer.parseInt(str[0]);
+            int num2 = Integer.parseInt(str[1]);
+            System.out.println(calculateSum(num1, num2));
+
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    private static int calculateSum(int num1, int num2) {
+        int sum = 0;
+        for (int i = num1; i < num2 +1 ; i++) {
+            if(isPrime(i)) sum += i;
+        }
+        return sum;
+    }
+
+    private static boolean isPrime(int n) {
+        for(int i = 2; i*i <= n; i++)
+            if(n % i == 0) return false;
+
+        return true;
+    }
+}
