de1e331916b25d177a03fe9df64e782afd33f08b
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-17 15:17:41 +0900

    Title: 계단 오르기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241017/계단 오르기/climbing-stairs.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(step(num));
+    }
+
+    public static int step(int n) {
+        int arr[] = new int[n+4];
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 1;
+
+        for (int i = 5; i <= n ; i++) {
+            arr[i] = arr[i-2] + arr[i-3];
+        }
+        return arr[n]%10007;
+    }
+}
