32112b3549ec20b43e562bc04723e036a56afbbb
Starry <102525066+starryboram@users.noreply.github.com>
2024-10-18 21:50:43 +0900

    Title: 사각형 채우기 3, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241018/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(fillInSquare(num));
+    }
+
+    public static int fillInSquare(int n) {
+        int[] arr = new int[n+2];
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 7;
+
+        if (n>=3) {
+            for (int i = 3 ; i <= n ; i++) {
+                arr[i] = (2 * arr[i-1] + 3 * arr[i-2])%1000000007;
+            }
+        }
+        return arr[n];
+    }
+}
