51df9bbcd5654fdbf03ad98f19adfc4b7522c550
Starry <102525066+starryboram@users.noreply.github.com>
2025-01-16 16:48:27 +0900

    Title: 아름다운 수열 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250116/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,72 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] arrA, arrB;
+    public static void main(String[] args) throws IOException {
+
+        readArr();
+
+        // 1. 수열B 를 가지고 전체적인 수열 완성
+        int[] beautifulArr = new int[M];
+        boolean[] checkArr = new boolean[M];
+        Set<List<Integer>> duplicationCheckSet = new HashSet<>();
+        makeBeautifulArr(0, beautifulArr, checkArr, duplicationCheckSet);
+
+        // 2. 완성된 '아름다운 수열'로 수열A 에 있는지 체크
+        int cnt = countBeautifulArr(duplicationCheckSet);
+        System.out.println(cnt);
+    }
+
+    private static int countBeautifulArr(Set<List<Integer>> duplicationCheckSet) {
+        int cnt = 0;
+        for(int i=0; i<N-M+1; i++) {
+            // i~i+M 까지의 수열
+            int[] result = Arrays.copyOfRange(arrA, i, i + M);
+            if(duplicationCheckSet.contains(Arrays.stream(result).boxed().toList())) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    private static void makeBeautifulArr(int digit, int[] beautifulArr, boolean[] checkArr, Set<List<Integer>> duplicationCheckSet) {
+        if(digit == M) {
+            duplicationCheckSet.add(Arrays.stream(beautifulArr).boxed().toList());
+            return;
+        }
+
+        for(int i=0; i<M; i++) {
+            if(checkArr[i]) continue;
+            checkArr[i] = true;
+            beautifulArr[digit] = arrB[i];
+            makeBeautifulArr(digit+1, beautifulArr, checkArr, duplicationCheckSet);
+            checkArr[i] = false;
+        }
+    }
+
+    private static void readArr() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arrA = new int[N];
+        for(int i=0; i<N; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        arrB = new int[M];
+        for(int i=0; i<M; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
