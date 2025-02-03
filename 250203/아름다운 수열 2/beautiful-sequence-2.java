b48a9033820069eebfaf8196c2368f7694afce6f
Starry <102525066+starryboram@users.noreply.github.com>
2025-02-03 15:42:08 +0900

    Title: 아름다운 수열 2, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 250203/아름다운 수열 2/beautiful-sequence-2.java
+++ 250203/아름다운 수열 2/beautiful-sequence-2.java
@@ -8,62 +8,42 @@ public class Main {
     static int N, M;
     static int[] arrA, arrB;
     static int cnt;
-    static Set<String> uniqueBeautifulSequences = new HashSet<>(); // 이미 확인한 아름다운 수열 저장
 
     public static void main(String[] args) throws IOException {
         readArr();
 
-        // 정렬된 아름다운 수열을 담기 위한 배열
-        int[] beautifulArr = new int[M];
-        boolean[] checkArr = new boolean[M];
+        // 아름다운 수열이라는 것은 결국 arrB 에 있는 원소의 빈도 개수로 판별하면 된다.
+        Map<Integer, Integer> need = new HashMap<>();
+        for(int num : arrB) {
+            need.put(num, need.getOrDefault(num, 0) + 1);
+        }
 
-        // 1. 모든 아름다운 수열 만들고, A 배열에서 찾기
-        makeBeautifulArr(0, beautifulArr, checkArr);
+
+        Map<Integer, Integer> diff = new HashMap<>();
+        int needKeySize = need.keySet().size();
+
+        for(int i=0; i<N-M+1; i++) {
+            diff.clear();
+            int diffCount = 0;
+            int[] arr = Arrays.copyOfRange(arrA, i, i + M);
+            for(int num : arr) {
+                diff.put(num, diff.getOrDefault(num, 0) + 1);
+            }
+            for(int num : diff.keySet()) {
+                if(Objects.equals(diff.get(num), need.get(num))){
+                    diffCount++;
+                }
+            }
+            if(diffCount == needKeySize) {
+                cnt++;
+            }
+        }
 
         System.out.println(cnt);
     }
 
-    private static void countBeautifulArr(int[] beautifulArr) {
-        // 배열을 정렬한 문자열로 변환하여 HashSet에 저장
-        String key = getStringKey(beautifulArr);
-        if (uniqueBeautifulSequences.contains(key)) return;
-        uniqueBeautifulSequences.add(key);
 
-        // 슬라이딩 윈도우 방식으로 arrA에서 beautifulArr와 동일한 부분 수열 찾기
-        for (int i = 0; i <= N - M; i++) {
-            if (checkSameArr(arrA, i, beautifulArr)) {
-                cnt++;
-            }
-        }
-    }
 
-    private static boolean checkSameArr(int[] arrA, int start, int[] beautifulArr) {
-        for (int i = 0; i < M; i++) {
-            if (arrA[start + i] != beautifulArr[i]) return false;
-        }
-        return true;
-    }
-
-    private static void makeBeautifulArr(int digit, int[] beautifulArr, boolean[] checkArr) {
-        if (digit == M) {
-            countBeautifulArr(beautifulArr);
-            return;
-        }
-
-        for (int i = 0; i < M; i++) {
-            if (!checkArr[i]) {
-                checkArr[i] = true;
-                beautifulArr[digit] = arrB[i];
-                makeBeautifulArr(digit + 1, beautifulArr, checkArr);
-                checkArr[i] = false;
-            }
-        }
-    }
-
-    private static String getStringKey(int[] arr) {
-        int[] sortedArr = arr.clone();
-        return Arrays.toString(sortedArr);
-    }
 
     private static void readArr() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
